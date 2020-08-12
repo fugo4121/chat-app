5bf101b2ed90d77825f732f4c62765297d2af84e
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-12 20:37:28 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # deviseのコントローラーから呼び出された場合、devise_parameter_sanitizer呼び出し
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # deviseでユーザーを登録時に使用でき、nameのカラムを許容する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
