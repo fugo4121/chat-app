ea9b6b913ca96d0338a4917e8a1a30b36f31db1a
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-17 22:31:46 +0900

    ユーザー情報表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ユーザー情報変更画面
+  # edit_user_path	GET	/users/:id/edit(.:format)	users#edit
+  
+  
   # / ルート への遷移先
   root "messages#index"
 
