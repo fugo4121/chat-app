19f73717837b0c2bc34c4dead3c1a0598da92dc8
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-17 22:51:41 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get 'messages/index'
 
   # ユーザー情報変更画面
-  # edit_user_path	GET	/users/:id/edit(.:format)	users#edit
+  # edit_user_path            GET     /users/:id/edit(.:format)	users#edit
+  # ユーザーログアウト
+  # destroy_user_session_path	DELETE  /users/sign_out(.:format)	devise/sessions#destroy
   
   
   # / ルート への遷移先
