edb023614257fc6d24eebfe40986c89b3bf75380
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-17 22:59:49 +0900

    バリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index'
 
+  # 新規アカウント作成画面
+  # app/views/registrations/new.html.erb
+
   # ユーザー情報変更画面
   # edit_user_path            GET     /users/:id/edit(.:format)	users#edit
+  # app/views/users/edit.html.erb
+
   # ユーザーログアウト
   # destroy_user_session_path	DELETE  /users/sign_out(.:format)	devise/sessions#destroy
-  
+  # app/views/users/edit.html.erb
   
   # / ルート への遷移先
   root "messages#index"
