477a82c79672e9a99aba6b0d7a055e5af1343a87
fugo4121 <lancerevo345@yahoo.co.jp>
2020-08-19 21:52:07 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,25 @@ Rails.application.routes.draw do
   # app/views/registrations/new.html.erb
 
   # ユーザー情報変更画面
-  # edit_user_path            GET     /users/:id/edit(.:format)	users#edit
+  # edit_user_path            GET     /users/:id/edit(.:format)          users#edit
   # app/views/users/edit.html.erb
 
   # ユーザーログアウト
-  # destroy_user_session_path	DELETE  /users/sign_out(.:format)	devise/sessions#destroy
+  # destroy_user_session_path	DELETE  /users/sign_out(.:format)          devise/sessions#destroy
   # app/views/users/edit.html.erb
 
   # 新規チャットルーム作成画面
-  # new_room_path             GET     /rooms/new(.:format)	rooms#new
+  # new_room_path             GET     /rooms/new(.:format)               rooms#new
   
+  # チャットルームごとの画面
+  # room_messages_path        GET     /rooms/:room_id/messages(.:format) messages#index
+
   # / ルート への遷移先
   root "rooms#index"
 
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   
 end
